84c205b74cf74c933ff3f9131b29a068d52cecb5
Raees-B-19 <planterx26@gmail.com>
2024-01-25 20:25:58 +0200

    Able to get Geographical Coordinates ( lat and lon ) from a form

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,20 +1,60 @@
 <script setup lang="ts">
-async function getWeather() {
-  let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=';
+const apiKey = "c71a382e51b62d85bce7fe1e6db2810f";
+const openWeatherMapUrl = "https://api.openweathermap.org";
 
+async function getWeather() {
+  let url = openWeatherMapUrl + "/data/2.5/forecast";
+
+  // Add onto url
+  url += "?lat=44.34&lon=10.99&appid=" + apiKey;
+
+  // Make Fetch
   await fetch(url)
-    .then((res) => res.json())
     .then((data) => console.log(data))
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
 
-getWeather()
+let locationModel = defineModel("");
+
+async function getGeographicalCoordinates() {
+  let url = openWeatherMapUrl + "/geo/1.0/direct";
+  let limit = 1;
+
+  // Get locationModel value
+  let location = `${locationModel.value}`;
+
+  // Replace spaces with underscore
+  location = location.replace(" ", "_");
+
+  // Add onto url
+  url += `?q=${location}&limit=${limit}&appid=${apiKey}`;
+
+  // Make Fetch
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
+}
 </script>
 
 <template>
   <div>
     <h2>Weather Forecast</h2>
 
-    <button v-on:click="getWeather">Get data</button>
+    <form @submit.prevent="getGeographicalCoordinates">
+      <div>
+        <label for="locationInput" class="block">Location</label>
+        <input
+          type="text"
+          id="locationInput"
+          minlength="1"
+          placeholder="Location"
+          v-model.trim="locationModel"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-success">Get data</button>
+    </form>
   </div>
 </template>
