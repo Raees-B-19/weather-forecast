0f6a27ad18f7efb7fb6877c501cab0de80c4f619
Raees-B-19 <planterx26@gmail.com>
2024-01-25 22:10:11 +0200

    Can get forecast data

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -2,21 +2,31 @@
 const apiKey = "c71a382e51b62d85bce7fe1e6db2810f";
 const openWeatherMapUrl = "https://api.openweathermap.org";
 
+let locationModel = defineModel("");
+
+interface GeographicalCoordinates {
+  lat: number;
+  lon: number;
+}
+
+// Get Weather
 async function getWeather() {
+  let coordinates: GeographicalCoordinates = await getGeographicalCoordinates();
+
   let url = openWeatherMapUrl + "/data/2.5/forecast";
 
-  // Add onto url
-  url += "?lat=44.34&lon=10.99&appid=" + apiKey;
+  // Add coordinates to url
+  url += `?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
 
   // Make Fetch
   await fetch(url)
+    .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.error("Error:", error));
 }
 
-let locationModel = defineModel("");
-
-async function getGeographicalCoordinates() {
+// Get Coordinates
+async function getGeographicalCoordinates(): Promise<GeographicalCoordinates> {
   let url = openWeatherMapUrl + "/geo/1.0/direct";
   let limit = 1;
 
@@ -30,10 +40,23 @@ async function getGeographicalCoordinates() {
   url += `?q=${location}&limit=${limit}&appid=${apiKey}`;
 
   // Make Fetch
-  await fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error:", error));
+  let response = await fetch(url);
+
+  // Check if ok
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  // Get json response
+  let data = await response.json();
+
+  // Get coordinates
+  let coordinates: GeographicalCoordinates = {
+    lat: data[0].lat,
+    lon: data[0].lon,
+  };
+
+  return coordinates;
 }
 </script>
 
@@ -41,7 +64,7 @@ async function getGeographicalCoordinates() {
   <div>
     <h2>Weather Forecast</h2>
 
-    <form @submit.prevent="getGeographicalCoordinates">
+    <form @submit.prevent="getWeather">
       <div>
         <label for="locationInput" class="block">Location</label>
         <input
