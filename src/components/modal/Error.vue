d35a75c70538e233df05d6cd1481975777c40ef6
Raees-B-19 <planterx26@gmail.com>
2024-02-01 17:16:17 +0200

    Updated design

---
+++ src/components/modal/Error.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// Props
+defineProps<{
+  locationName: string
+}>()
+</script>
+
+<template>
+    <dialog id="error-modal" class="modal">
+      <div class="modal-box border-2 border-rose-500">
+        <h3 class="font-bold text-lg text-rose-300 mb-3">Error</h3>
+
+        <p class="mb-3">
+          Unable to get weather forecast for {{ locationName }}.
+        </p>
+
+        <p>
+          Please enter a known location
+          <span class="font-bold">e.g Cape Town</span>.
+        </p>
+
+        <p class="py-3">Press ESC key or click outside to close.</p>
+      </div>
+
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+</template>
--- src/views/WeatherV.vue
+++ src/views/WeatherV.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { useWeatherStore } from "@/stores/weather";
 import FavoriteLocation from "@/components/card/FavoriteLocation.vue";
+
+// Icon
+import IconSearch from "@/components/icons/SearchBar.vue";
+import IconStar from "@/components/icons/Star.vue";
+
+// Modals
 import ModalDocumentation from "@/components/modal/Documentation.vue";
-import IconDocumentation from "@/components/icons/IconDocumentation.vue";
+import ModalError from "@/components/modal/Error.vue";
 
 // Interfaces
 import { type GeographicalCoordinates, type LocationData } from "@/interfaces";
@@ -10,8 +16,6 @@ import { type GeographicalCoordinates, type LocationData } from "@/interfaces";
 const weatherStore = useWeatherStore();
 
 let locationModel = defineModel("");
-
-let currentLocationName = "";
 
 // Store
 weatherStore.currentLocationData = {
@@ -64,8 +68,6 @@ async function handleSubmit() {
     // If not in favorite locations
     if (!weatherStore.checkIfInFavoriteLocations()) {
       // Display save location button
-      currentLocationName = `${locationModel.value}`;
-
       weatherStore.displaySaveLocationButton = true;
     }
   } catch (error) {
@@ -85,97 +87,62 @@ function handleSaveLoaction() {
 
   locationModel.value = "";
 }
-
-function handleShowDocumentationModal() {
-  let documentationModal = document.getElementById("documentation-modal") as HTMLDialogElement;
-
-  // Show the documentation modal
-  documentationModal.showModal();
-}
 </script>
 
 <template>
-  <div>
-    <div @click="handleShowDocumentationModal">
-      <IconDocumentation />
-    </div>
-
-    <ModalDocumentation />
-
-    <div class="grid lg:grid-cols-12 sm:grid-cols-12">
-      <!-- Your content goes here -->
-      <div class="mx-auto lg:col-span-6 sm:col-span-6">
-        <form @submit.prevent="handleSubmit">
-          <div>
-            <label for="locationInput" class="block">Location</label>
-            <input
-              type="text"
-              id="locationInput"
-              minlength="1"
-              placeholder="Location"
-              v-model.trim="locationModel"
-              required
-            />
-          </div>
-
-          <button type="submit" class="btn btn-success">Get data</button>
-        </form>
-
-        <div v-if="weatherStore.displaySaveLocationButton">
-          <button
-            type="button"
-            class="btn btn-primary"
-            @click="handleSaveLoaction"
-          >
-            Save Location {{ currentLocationName }}
-          </button>
-        </div>
-      </div>
-
-      <div class="mx-auto lg:col-span-6 sm:col-span-6">
-        <div
-          v-for="locationData in weatherStore.favoriteLocations"
-          :key="locationData.locationName"
-        >
-          <FavoriteLocation
-            :country="locationData.country"
-            :locationName="locationData.locationName"
-            :lat="locationData.lat"
-            :lon="locationData.lon"
-            @getWeatherForecast="
-              weatherStore.getWeatherForecast(
-                locationData.lat,
-                locationData.lon
-              )
-            "
-            @removeFavoriteLocation="
-              weatherStore.removeFavoriteLocation(locationData.locationName)
-            "
+  <div class="grid sm:grid-cols-12 lg:grid-cols-12">
+    <div class="sm:col-span-6 lg:col-span-6">
+      <form @submit.prevent="handleSubmit">
+        <div class="flex">
+          <input
+            type="text"
+            id="locationInput"
+            class="input input-bordered"
+            minlength="1"
+            placeholder="Search City"
+            v-model.trim="locationModel"
+            required
           />
+
+          <button type="submit" class="btn btn-ghost btn-circle ml-1">
+            <IconSearch />
+          </button>
+
+          <div v-if="weatherStore.displaySaveLocationButton">
+            <button
+              type="button"
+              class="btn btn-ghost btn-circle ml-1"
+              @click="handleSaveLoaction"
+            >
+              <IconStar />
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
 
-    <!-- Error Dialog -->
-    <dialog id="error-modal" class="modal">
-      <div class="modal-box border-2 border-rose-500">
-        <h3 class="font-bold text-lg text-rose-300 mb-3">Error</h3>
-
-        <p class="mb-3">
-          Unable to get weather forecast for {{ locationModel }}
-        </p>
-
-        <p>
-          Please enter a known location
-          <span class="font-bold">e.g Cape Townc</span>
-        </p>
-
-        <p class="py-3">Press ESC key or click outside to close</p>
+    <div class="mx-auto sm:col-span-6 lg:col-span-6">
+      <div
+        v-for="locationData in weatherStore.favoriteLocations"
+        :key="locationData.locationName"
+      >
+        <FavoriteLocation
+          :country="locationData.country"
+          :locationName="locationData.locationName"
+          :lat="locationData.lat"
+          :lon="locationData.lon"
+          @getWeatherForecast="
+            weatherStore.getWeatherForecast(locationData.lat, locationData.lon)
+          "
+          @removeFavoriteLocation="
+            weatherStore.removeFavoriteLocation(locationData.locationName)
+          "
+        />
       </div>
-
-      <form method="dialog" class="modal-backdrop">
-        <button>close</button>
-      </form>
-    </dialog>
+    </div>
   </div>
+
+  <ModalDocumentation />
+
+  <ModalError :locationName="`${locationModel}`" />
 </template>
