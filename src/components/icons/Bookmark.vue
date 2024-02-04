e0e9a40de6e16b002cc98b0b96466ed2b401f013
Raees-B-19 <planterx26@gmail.com>
2024-02-04 20:30:28 +0200

    Updated icons folder

--- src/components/icons/Bookmark.vue
+++ src/components/icons/Bookmark.vue
@@ -1,10 +1,10 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="20"
+    height="21"
     viewBox="0 0 24 24"
-    style="fill: #8f99ac; transform: ; msfilter: "
+    style="fill: currentColor; transform: ; msfilter: "
   >
     <path
       d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"
