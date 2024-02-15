d1fbd7beeb8b8ff99354a0b46ecb962f5ffb2fa1
Raees-B-19 <planterx26@gmail.com>
2024-02-15 10:48:46 +0200

    Added footer with copyright

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+
+// Get current year
+let currentYear = new Date().getFullYear();
 </script>
 
 <template>
   <div class="container mx-auto mt-4 p-2 md:w-4/5">
     <RouterView />
   </div>
+
+  <footer>
+    <p class="text-center">&copy; Copyright {{ currentYear }}</p>
+  </footer>
 </template>
 
 <style scoped></style>
